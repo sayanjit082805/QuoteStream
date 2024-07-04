52a99294a29ba9247c85b4ab0cf1e83eb32bc797
sayanjit082805 <sayanjitukil2005@gmail.com>
2024-07-04 11:01:13 +0530

    update font settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter"],
+      },
       boxShadow: {
         "3xl": "0 2px 8px rgba(0, 0, 0, 0.6)",
       },
